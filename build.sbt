619683e4ddbb5dad926f4fdb9c98dfc4692f95ca
Sahil Gogna <gogna.sahil95@gmail.com>
2020-09-07 12:38:01 -0400

    Kafka topic streamed using spark streaming

--- build.sbt
+++ build.sbt
@@ -2,14 +2,21 @@ name := "Pharmacodynamics"
 
 version := "0.1"
 
-scalaVersion := "2.11.8"
+scalaVersion := "2.12.10"
 
 val hadoopVersion = "2.7.3"
 
-val sparkVersion = "2.3.3"
-
+val sparkVersion = "3.0.0"
+val kafkaVersion = "2.4.0"
 val akkaVersion = "2.5.24"
 val akkaHttpVersion = "10.1.7"
+val log4jVersion = "2.4.1"
+
+resolvers ++= Seq(
+  "bintray-spark-packages" at "https://dl.bintray.com/spark-packages/maven",
+  "Typesafe Simple Repository" at "https://repo.typesafe.com/typesafe/simple/maven-releases",
+  "MavenRepository" at "https://mvnrepository.com"
+)
 
 libraryDependencies ++= Seq(
   "org.apache.hadoop" % "hadoop-common" % hadoopVersion,
@@ -18,15 +25,26 @@ libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % sparkVersion,
   "org.apache.spark" %% "spark-sql" % sparkVersion,
   "org.apache.spark" %% "spark-streaming" % sparkVersion,
-  // streaming-kafka
-  "org.apache.spark" %% "spark-streaming-kafka-0-10" % "2.3.3",
+
+
+  "org.apache.spark" %% "spark-streaming-kafka-0-10" % sparkVersion,
+
+  "org.apache.spark" %% "spark-sql-kafka-0-10" % "3.0.0",
+
+  "org.apache.kafka" % "kafka-streams" % kafkaVersion,
+  "org.apache.kafka" % "kafka-clients" % kafkaVersion,
 
   // akka
   "com.typesafe.akka" %% "akka-remote" % akkaVersion,
   "com.typesafe.akka" %% "akka-stream" % akkaVersion,
   "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
+
+  // logging
+  "org.apache.logging.log4j" % "log4j-api" % log4jVersion,
+  "org.apache.logging.log4j" % "log4j-core" % log4jVersion,
+
+
+  "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.11.0",
+  "com.fasterxml.jackson.core" % "jackson-databind" % "2.11.0",
+  "com.fasterxml.jackson.core" % "jackson-core" % "2.11.0"
 )
-
-lazy val excludeJpountz = ExclusionRule(organization = "net.jpountz.lz4", name = "lz4")
-
-lazy val kafkaClients = "org.apache.kafka" % "kafka-clients" % "2.3.3" excludeAll (excludeJpountz)
---
+++ src/main/scala/ca/science/spark/SparkAggregator.scala
@@ -0,0 +1,91 @@
+package ca.science.spark
+
+import org.apache.spark.sql.streaming.{GroupState, GroupStateTimeout, OutputMode}
+import org.apache.spark.sql.{Dataset, SparkSession}
+
+object SparkAggregator {
+
+  val spark = SparkSession.builder()
+    .appName("Pharmacodynamics")
+    .master("local[2]")
+    .getOrCreate()
+
+  spark.sparkContext.setLogLevel("ERROR")
+
+  import spark.implicits._
+
+  case class UserResponse(sessionId: String, responseTime: Long)
+
+  case class UserAvgResponse(sessionId: String, avgDuration: Double)
+
+  def readUserResponse(): Dataset[UserResponse] = spark.readStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", "localhost:9092")
+    .option("subscribe", "science1")
+    .load()
+    .select("value")
+    .as[String]
+    .map { line =>
+      val tokens = line.split(",")
+      val sessionId = tokens(0)
+      val time = tokens(1).toLong
+
+      UserResponse(sessionId, time)
+    }
+
+  def updateUserResponseTime
+  (n: Int)
+  (sessionId: String, group: Iterator[UserResponse], state: GroupState[List[UserResponse]])
+  : Iterator[UserAvgResponse] = {
+    group.flatMap { record =>
+      val lastWindow =
+        if (state.exists) state.get
+        else List()
+
+      val windowLength = lastWindow.length
+      val newWindow =
+        if (windowLength >= n) lastWindow.tail :+ record
+        else lastWindow :+ record
+
+      // for Spark to give us access to the state in the next batch
+      state.update(newWindow)
+
+      if (newWindow.length >= n) {
+        val newAverage = newWindow.map(_.responseTime).sum * 1.0 / n
+        Iterator(UserAvgResponse(sessionId, newAverage))
+      } else {
+        Iterator()
+      }
+    }
+  }
+
+  def getAverageResponseTime(n: Int) = {
+    readUserResponse()
+      .groupByKey(_.sessionId)
+      .flatMapGroupsWithState(OutputMode.Append, GroupStateTimeout.NoTimeout())(updateUserResponseTime(n))
+      .map{
+        userRes =>
+          if(userRes.avgDuration > 1500) s"${userRes.sessionId} is not Attentive"
+          else s"${userRes.sessionId} is Attentive"
+      }
+      .writeStream
+      .format("console")
+      .outputMode("append")
+      .option("truncate", false)
+      .start()
+      .awaitTermination()
+  }
+
+  def logUsers() = {
+    readUserResponse().writeStream
+      .format("console")
+      .outputMode("append")
+      .start()
+      .awaitTermination()
+  }
+
+  def main(args: Array[String]): Unit = {
+    getAverageResponseTime(3)
+  }
+
+}
